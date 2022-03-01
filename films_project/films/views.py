3a85ac630f3e3a23785229b33da32ab430b3f19b
Dieudonne Munganga <dieudonneishara@gmail.com>
2022-03-01 13:24:52 +0200

    query parameters added and tested

--- films_project/films/views.py
+++ films_project/films/views.py
@@ -18,13 +18,31 @@ def main(request):
 
 
 def user_info(request):
-    userinfo = {
-        'username': 'Fabrício', # Put your name here
-        'country': 'Brazil', # Put your country here
-    }
-    context = {'userinfo': userinfo,
-               'title': 'User Info Page'}
+    # userinfo = {
+    #     'username': 'Fabrício', # Put your name here
+    #     'country': 'Brazil', # Put your country here
+    # }
+    # context = {'userinfo': userinfo,
+    #            'title': 'User Info Page'}
     
-    return render(request,
-                  'films/user_info.html',
-                  context)
+    # return render(request,
+    #               'films/user_info.html',
+    #               context)
+    if request.method == 'GET':
+        # remove this print later
+        print('\n\nrequest.GET ==>>',
+              request.GET,
+              '\n\n')
+    
+        userinfo = {
+            'username': request.GET.get('username'),
+            'country': request.GET.get('country'),
+        }
+        context = {'userinfo': userinfo,
+                   'title': 'User Info Page'}
+        template = 'films/user_info.html'
+        return render(request,
+                      template,
+                      context)
+    # elif request.method == 'POST':
+    #     return HttpResponse('POST request here.')
